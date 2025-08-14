1435e0cf0fc1da54bf74fb20dbb74a863dfe8c1e
Myriam Jara <161982232+myriammmm13@users.noreply.github.com>
2025-08-14 16:18:53 -0600

    final ejercicio2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,24 +1,24 @@
 """
 Escriba un programa en Python que permita guardar y 
 recuperar de un archivo binario, una lista de diccionarios 
-(ustedes son libres de crear la estructura y agregar los datos 
+(ustedes son libres de crear la
+estructura y agregar los datos 
 que quieran, siempre y cuando sea una lista de diccionarios). 
 """
-import pickle
+import pickle #libreria usada por el profe en los videos :)
 list_dic=[]
 list_dic.append({"Nombre":"Myriam", "Apodo":"Gemela", "Edad": "19", "Residencia":"Liberia"})
 list_dic.append({"Nombre":"Mariam", "Apodo":"Gemela", "Edad": "19", "Residencia":"San Jose"})
 list_dic.append({"Nombre":"Sara", "Apodo":"Ma", "Edad": "54", "Residencia":"Bijagua"})
 list_dic.append({"Nombre":"Tiana", "Apodo":"Titi", "Edad": "31", "Residencia":"Heredia"})
 
-def lectura(archivo):
-    with open(archivo, "wb") as AB:
-        pickle.dump(list_dic, AB)
 
-def esritura(archivo):
-    pass
+with open("lista.bin", "wb") as archivo_binario:
+    pickle.dump(list_dic, archivo_binario)
 
+with open("lista.bin", "rb") as archivo_binario:
+    rec = pickle.load(archivo_binario)
 
-
-lectura("archivo.bin")
-
+print("Lista de personas que hay en el archivo:")
+for diccionario in rec:
+    print(diccionario)
